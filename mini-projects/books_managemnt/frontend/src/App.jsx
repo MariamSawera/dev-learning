eebc470e82ad4d5f0dd39d30363de0c87a51db5b
MariamSawera <mariamsawera@gmail.com>
2026-01-14 17:51:53 +0500

    added frontend for login

--- mini-projects/books_managemnt/frontend/src/App.jsx
+++ mini-projects/books_managemnt/frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
-    <div>
       <Routes>
-        <Route path="/" element={LoginPage}/>
-        {/* <Route/> */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
 
       </Routes>
-    </div>
   )
 }
 
--- mini-projects/books_managemnt/frontend/src/pages/LoginPage.jsx
+++ mini-projects/books_managemnt/frontend/src/pages/LoginPage.jsx
@@ -1,20 +1,81 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import api from "../lib/axios"
 
 const LoginPage = () => {
+
+  const [isSignup, setIsSignup] = useState(false)
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const [loading, setLoading] = useState(false);
+      const [error, setError] = useState('')
+
+
     // useEffect(() => {
     //     const fetch
     // })
-    const handleSubmit = () => {
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      setError("");
 
+      try {
+        if(isSignup) {
+          const res = await api.post("/auth/register", {
+            name: name,
+            email: email,
+            password:password,
+          })
+
+          console.log("Register success", res.data)
+        } 
+
+        else{
+          const res = await api.post("/auth/login", {
+            email:email,
+            password:password,
+          })
+          console.log("Login susccess", res.data)
+        }
+
+      }catch(error){
+              setError('Invalid credentials or server error')
+
+
+      }
+        
+     setLoading(false)
 
     }
+
   return (
     <div>
+      <h2>{isSignup ?"Signup" : "Login"}</h2>
+      <form onSubmit={handleSubmit}>
+        {isSignup && (
+          <input type="text" value={name} placeholder='name' onChange={(e) => setName(e.target.value)} />
+        )}
+        <br />
+          <input type="text" value={email} placeholder='email' onChange={(e) => setEmail(e.target.value)} />
+        <br />
+          <input type="text" value={password} placeholder='password' onChange={(e) => setPassword(e.target.value)} />
+        <br />
+
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+
+        <button type='submit'>
+          {
+            loading ? "Loading..." : isSignup ? "Sign Up" : "Login"
+          }
+        </button>
+      </form>
+
+        <button onClick={() => setIsSignup(!isSignup)}>
+        {isSignup ? 'Go to Login' : 'Go to Sign Up'}
+      </button>
+
       
     </div>
   )
